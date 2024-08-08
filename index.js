a21347d8b7634e3361009753196a688f2f19e882
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-08 19:10:28 +0600

    setup chef api

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT ||  3000
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/chefs', (req, res)=> {
+    res.send(chefs);
+})
+
 app.listen(port, () => {
     console.log(`Chef recipe hunter server is running on server: ${port}`)
 })
