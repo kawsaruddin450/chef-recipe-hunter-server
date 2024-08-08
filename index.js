d12c80486e176a51e7ec2aeea2fdbeb9f5d5e349
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-08 23:54:52 +0600

    chef.json image link update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT ||  3000
 const chefs = require('./data/chefs.json');
 const recipes = require('./data/recipes.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
